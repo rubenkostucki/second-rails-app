777e046fb363b2e10e1e1a196ee330b37b28b01f
Ruben Kostucki <rubenkostucki@gmail.com>
2013-04-08 17:13:09 +0100

    second attempt to my first rails app with scaffold user generator. database not working yet

---
+++ second-app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondApp::Application.config.secret_token = 'ec60287894862bf2db6789a77fca850674ddb7f6144101483bd50173487f82bd16c0008f3266a7ba7100f2e87d532429d254e69f10cf6ad94c02f81676692966'
